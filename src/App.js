55d85cffdf10861be7e4cc2ce4fc8a3cfbce62cd
P K Agrawal <pranshukumar1999@gmail.com>
2022-02-26 10:50:25 +0530

    Metamask wallet authentication added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, {useEffect} from "react";
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,12 +9,32 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }
+
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+
+   useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="App">
